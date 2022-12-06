97851106f03bcc90e4f760bea2900a8e6543a322
TrangNhung19 <94165318+TrangNhung19@users.noreply.github.com>
2022-12-07 00:50:03 +0700

    Update main.go

--- project/main.go
+++ project/main.go
@@ -23,6 +23,25 @@ func productHandler(w http.ResponseWriter, r *http.Request) {
 func cartHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.ExecuteTemplate(w, "cart.html", nil)
 }
+func product_dtHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl.ExecuteTemplate(w, "product_detail.html", nil)
+}
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl.ExecuteTemplate(w, "about.html", nil)
+}
+func checkoutHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl.ExecuteTemplate(w, "checkout.html", nil)
+}
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl.ExecuteTemplate(w, "contact.html", nil)
+}
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl.ExecuteTemplate(w, "register.html", nil)
+}
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl.ExecuteTemplate(w, "search.html", nil)
+}
+
 func main() {
 	fs := http.FileServer(http.Dir("assets"))
 	http.Handle(("/assets/"), http.StripPrefix("/assets", fs))
@@ -31,8 +50,13 @@ func main() {
 
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/login.html", loginHandler)
+	http.HandleFunc("/register.html", registerHandler)
 	http.HandleFunc("/cart.html", cartHandler)
 	http.HandleFunc("/products.html", productHandler)
+	http.HandleFunc("/product_dt.html", product_dtHandler)
+	http.HandleFunc("/about.html", aboutHandler)
+	http.HandleFunc("/contact.html", contactHandler)
+	http.HandleFunc("/checkout.html", checkoutHandler)
 
 	http.ListenAndServe(":5000", nil)
 }
